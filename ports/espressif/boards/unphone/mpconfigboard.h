f06c6addfbd9554de39cb628a0401686fe903cac
Hamish Cunningham <hamish@gate.ac.uk>
2023-01-15 21:12:47 +0000

    better set of pin defs for unphone

--- ports/espressif/boards/unphone/mpconfigboard.h
+++ ports/espressif/boards/unphone/mpconfigboard.h
@@ -29,19 +29,29 @@
 #define MICROPY_HW_BOARD_NAME       "unPhone"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
-#define MICROPY_HW_NEOPIXEL (&pin_GPIO33)
-#define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO21)
+//#define MICROPY_HW_NEOPIXEL (&pin_GPIO33)
+//#define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO21)
+#define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO13)
 
 #define MICROPY_HW_LED_STATUS (&pin_GPIO13)
 
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO4)
 #define DEFAULT_I2C_BUS_SDA (&pin_GPIO3)
 
+/*
 #define DEFAULT_SPI_BUS_SCK (&pin_GPIO36)
 #define DEFAULT_SPI_BUS_MOSI (&pin_GPIO35)
 #define DEFAULT_SPI_BUS_MISO (&pin_GPIO37)
 
 #define DEFAULT_UART_BUS_RX (&pin_GPIO38)
 #define DEFAULT_UART_BUS_TX (&pin_GPIO39)
+*/
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO39)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO40)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO41)
 
-#define DOUBLE_TAP_PIN (&pin_GPIO34)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO44)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO43)
+
+// #define DOUBLE_TAP_PIN (&pin_GPIO34)
+#define DOUBLE_TAP_PIN (&pin_GPIO45)
--- ports/espressif/boards/unphone/pins.c
+++ ports/espressif/boards/unphone/pins.c
@@ -46,27 +46,34 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_A0), MP_ROM_PTR(&pin_GPIO18) },
     { MP_ROM_QSTR(MP_QSTR_D18), MP_ROM_PTR(&pin_GPIO18) },
 
+/*
     { MP_ROM_QSTR(MP_QSTR_NEOPIXEL_POWER), MP_ROM_PTR(&pin_GPIO21) },
     { MP_ROM_QSTR(MP_QSTR_NEOPIXEL), MP_ROM_PTR(&pin_GPIO33) },
+*/
 
-    { MP_ROM_QSTR(MP_QSTR_MOSI), MP_ROM_PTR(&pin_GPIO35) },
+//  { MP_ROM_QSTR(MP_QSTR_MOSI), MP_ROM_PTR(&pin_GPIO35) },
+    { MP_ROM_QSTR(MP_QSTR_MOSI), MP_ROM_PTR(&pin_GPIO40) },
     { MP_ROM_QSTR(MP_QSTR_D35), MP_ROM_PTR(&pin_GPIO35) },
 
-    { MP_ROM_QSTR(MP_QSTR_SCK), MP_ROM_PTR(&pin_GPIO36) },
+//  { MP_ROM_QSTR(MP_QSTR_SCK), MP_ROM_PTR(&pin_GPIO36) },
+    { MP_ROM_QSTR(MP_QSTR_SCK), MP_ROM_PTR(&pin_GPIO39) },
     { MP_ROM_QSTR(MP_QSTR_D36), MP_ROM_PTR(&pin_GPIO36) },
 
-    { MP_ROM_QSTR(MP_QSTR_MISO), MP_ROM_PTR(&pin_GPIO37) },
+//  { MP_ROM_QSTR(MP_QSTR_MISO), MP_ROM_PTR(&pin_GPIO37) },
+    { MP_ROM_QSTR(MP_QSTR_MISO), MP_ROM_PTR(&pin_GPIO41) },
     { MP_ROM_QSTR(MP_QSTR_D37), MP_ROM_PTR(&pin_GPIO37) },
 
 
-    { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_GPIO38) },
+//  { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_GPIO38) },
+    { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_GPIO44) },
     { MP_ROM_QSTR(MP_QSTR_D38), MP_ROM_PTR(&pin_GPIO38) },
 
-    { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_GPIO39) },
+//  { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_GPIO39) },
+    { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_GPIO43) },
     { MP_ROM_QSTR(MP_QSTR_D39), MP_ROM_PTR(&pin_GPIO39) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
-    { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
+//  { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) }
 };
